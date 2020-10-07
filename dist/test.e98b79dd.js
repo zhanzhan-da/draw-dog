0a57e249ba69602a061578f8d5606b617df2b829
zhanzhan-da <m13715332956@163.com>
2020-10-07 22:10:23 +0800

    修改标题

--- dist/test.e98b79dd.js
+++ dist/test.e98b79dd.js
@@ -211,7 +211,7 @@ player.init(); // let n = 1
 //   demo.innerText = string.substr(0, n)
 //   demo2.innerHTML = string.substr(0, n)
 // }, 300)
-},{"./css.js":"css.js"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./css.js":"css.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -239,7 +239,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52689" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54063" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -415,5 +415,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","test.js"], null)
+},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","test.js"], null)
 //# sourceMappingURL=/test.e98b79dd.js.map
